daa602deb644414f86e5332b00987ae82aeea82e
mamatha1999 <mamathakusuma.pb@gmail.com>
2026-03-31 23:13:07 +0530

    Server.js updated

--- backend/server.js
+++ backend/server.js
@@ -2,34 +2,30 @@ import express from "express";
 import dotenv from "dotenv";
 import todoRoutes from "./routes/todo.route.js";
 import { connectDB } from "./config/db.js";
-import cors from "cors";
 import path from "path";
 
-//Dotenv cofig should be at the top of the file before using any environment variables
 dotenv.config();
 
 const app = express();
-
 const __dirname = path.resolve();
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  // app.get("*", (req, res) => {
-  //   res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-  // });
-  // Only fallback for non-file routes
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-  });
-}
 
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
+// API routes
 app.use("/api/todos", todoRoutes);
+
+// Serve frontend
+app.use(express.static(path.join(__dirname, "frontend/dist")));
+
+app.get("*", (req, res) => {
+  if (req.path.startsWith("/api")) return;
+
+  res.sendFile(path.join(__dirname, "frontend/dist/index.html"));
+});
 
 app.listen(PORT, () => {
   connectDB();
-  console.log("Server started at http://localhost:5000");
+  console.log(`Server running on port ${PORT}`);
 });
